b26fc502ba9bbf900d5464901c8ca97d3215ae04
ShaulLavochkin <157960346+ShaulLavochkin@users.noreply.github.com>
2024-10-22 15:40:11 +0300

    initial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,76 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, Linking, Pressable, StyleSheet, Text } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ParallaxScrollView from '@/components/ParallaxScrollView'
+import { ThemedView } from '@/components/ThemedView'
+
+const call2 = 'phoenixinvest://'
+
+const call =
+	'pinfoapp://onelink/sso/phoenixinvest/305513459/d1d94907ecf83b82dff3e216dadb359231fc9433788228e5d5404e798ed050999d2b7430b605cf85c2f8f977abf701392a8064a0d8712d355ff953e5789cb92100470793d1b3e46702c0aa3ce436d48209789041159dc09fd8ae02c8c397568b2ce6f55c9dc03480fc497cbf26d0ed0e1a6ba0754e54701ce67062640fe61f64 f2d806a4145f00f15f34c02b27a271ea4fe9bf0d466622dc8a98e50a1b087d7/1729583670'
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	return (
+		<ParallaxScrollView
+			headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+			headerImage={
+				<Image
+					source={require('@/assets/images/partial-react-logo.png')}
+					style={styles.reactLogo}
+				/>
+			}
+		>
+			<ThemedView style={styles.titleContainer}>
+				<Pressable
+					onPress={async () => {
+						const can = await Linking.canOpenURL(call)
+						console.log({ can })
+						try {
+							await Linking.openURL(call)
+						} catch (e) {
+							console.log(e)
+							try {
+								await Linking.openURL(call)
+							} catch (e) {
+								console.log(e)
+							}
+						}
+					}}
+					style={styles.button}
+				>
+					<Text style={styles.buttonText}>OPEN DEEP LINK</Text>
+				</Pressable>
+			</ThemedView>
+		</ParallaxScrollView>
+	)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+	titleContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 8
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8
+	},
+	reactLogo: {
+		height: 178,
+		width: 290,
+		bottom: 0,
+		left: 0,
+		position: 'absolute'
+	},
+	button: {
+		backgroundColor: '#007bff',
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		borderRadius: 5,
+		alignItems: 'center'
+	},
+	buttonText: {
+		color: '#fff',
+		fontWeight: 'bold',
+		fontSize: 16
+	}
+})
